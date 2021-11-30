37833b53c233b8912ef3bb5da49688179acd1ed7
PoleEast <a1525123@gmail.com>
2021-11-30 14:56:38 +0800

    Initial commit

---
+++ Assets/script/HitBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public int damage;
+    public float STime, ETime;
+    private Animator myAnim;
+    private PolygonCollider2D hitbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        hitbox = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attack();
+    }
+    void attack()
+    {
+        if (Input.GetButtonDown("attack") && myAnim.GetCurrentAnimatorStateInfo(0).IsName("sword") == false)
+        {
+            myAnim.SetTrigger("attack");
+            StartCoroutine(enablehitbox());
+        }
+    }
+
+    IEnumerator disablehitbox()
+    {
+        yield return new WaitForSeconds(ETime);
+        hitbox.enabled = false;
+    }
+    IEnumerator enablehitbox()
+    {
+        yield return new WaitForSeconds(STime);
+        hitbox.enabled = true;
+        StartCoroutine(disablehitbox());
+    }
+}
---
+++ Assets/script/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float Runspeed;
+    public float Jumpspeed;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnim;
+    private BoxCollider2D myFeet;
+    private bool isGround;
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        run();
+        flip();
+        jump();
+        checkGround();
+        switchAnimation();
+    }
+    private void checkGround()
+    {
+        isGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+    private void flip()
+    {
+        bool playerHasXSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasXSpeed)
+        {
+            if (myRigidbody.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            if (myRigidbody.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+    private void run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(moveDir * Runspeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVel;
+        bool playerHasXSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnim.SetBool("run", playerHasXSpeed);
+    }
+    private void jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGround == true)
+        {
+            myAnim.SetBool("jump", true);
+            myAnim.SetBool("idle", false);
+            Vector2 jumpVel = new Vector2(0.0f, Jumpspeed);
+            myRigidbody.velocity = Vector2.up * jumpVel;
+        }
+    }
+    private void switchAnimation()
+    {
+        if (myAnim.GetBool("jump"))
+        {
+            if (myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) && myRigidbody.velocity.y < 0.1f)
+            {
+                myAnim.SetBool("jump", false);
+                myAnim.SetBool("idle", true);
+            }
+        }
+    }
+    // private void attack()
+    // {
+    //     if (Input.GetButtonDown("attack") && myAnim.GetCurrentAnimatorStateInfo(0).IsName("sword") == false)
+    //     {
+    //         myAnim.SetTrigger("attack");
+    //     }
+    // }
+}
+
