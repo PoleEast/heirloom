a4ed4f1bce1564974a9a45685d1c79195cc30310
PoleEast <a1525123@gmail.com>
2021-12-03 10:22:12 +0800

    add enemy flash by attack
    enemy flash by attack

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -6,10 +6,15 @@ public class Enemy : MonoBehaviour
 {
     public int Hp;
     public int Damage;
+    public float flashTime;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
     // Start is called before the first frame update
     public void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+        Debug.Log("st");
     }
 
     // Update is called once per frame
@@ -23,5 +28,15 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         Hp = Hp - damage;
+        flashColor(flashTime);
+    }
+    void flashColor(float time)
+    {
+        spriteRenderer.color = Color.red;
+        Invoke("resetcolor", time);
+    }
+    void resetcolor()
+    {
+        spriteRenderer.color = originalColor;
     }
 }
