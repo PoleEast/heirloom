2bedfefcfcad9e3afb3e5992f9785ecb01b42398
PoleEast <a1525123@gmail.com>
2021-12-03 10:35:20 +0800

    add camera follow
    camera follow with player

---
+++ Assets/script/Camerafllow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafllow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            if (transform.position != target.position)
+            {
+                Vector3 targetPosition = target.position;
+                transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
